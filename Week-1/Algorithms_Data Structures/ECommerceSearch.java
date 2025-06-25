c8a5f9dd92a92245b59e730669be8151cfb202a5
Testing-bot10 <santoonTV123@gmail.com>
2025-06-25 20:59:03 +0530

    Update ECommerceSearch.java

--- Week-1/Algorithms_Data Structures/ECommerceSearch.java
+++ Week-1/Algorithms_Data Structures/ECommerceSearch.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
-
+import java.util.*;
 public class ECommerceSearch {
       public static void main(String[] args) {
             Product[] products = {
